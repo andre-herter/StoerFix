903cce27bba3cdee7cc6fb34d9468975d6ba7c2d
Andre Herter <andre-herter@web.de>
2026-01-28 18:07:36 +0100

    Fix: wrap Header and Footer with HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { HashRouter } from "react-router-dom";
-
-const basename = import.meta.env.PROD ? "/StoerFix" : "";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter basename={basename}>
-      <App />
-    </HashRouter>
+    <App />
   </StrictMode>,
 );
